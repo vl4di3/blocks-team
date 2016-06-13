e8810c5cb306d56b8d9e00a93ea0ef71c4e53e5f
vl4di3 <thespartandos@gmail.com>
2016-06-13 08:37:03 +0200

    Update postaci.h

--- postaci.h
+++ postaci.h
@@ -11,6 +11,7 @@ class Postac :public sf::Drawable, sf::Transformable
     string klasa;
     int sila;
     char kierunek;
+    int odliczanie;
 
 public:
     bool IsHitting;
@@ -23,7 +24,7 @@ public:
     sf::Texture teksturacien;
     sf::Sprite cien;
 
-    Postac(string="brak",int=100,int=10,int=200,int=200, char='s',bool=false);
+    Postac(string="brak",int=100,int=10,int=200,int=200, char='s',bool=false,int=0);
 
     void stworz();
     void ruch(Wrog*enemy,Mapa*las1);
